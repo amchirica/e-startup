89d534d2c00649895289eefff6a9fa758d38983d
DariusMan22 <dariusmanolescu@yahoo.ro>
2023-04-08 14:54:01 +0300

    Main

--- app.py
+++ app.py
@@ -17,13 +17,29 @@ def homeindex():
 def blogslist():
     return render_template('blogslist.html')
 
-@app.route('/blogpost.html')
+@app.route('/blog.html')
 def blogspot():
-    return render_template('blogpost.html')
+    return render_template('blog.html')
 
 @app.route('/about.html')
 def about():
     return render_template('about.html')
+
+app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+app.route('/project.html')
+def project():
+    return render_template('project.html')
+
+app.route('/policy.html')
+def policy():
+    return render_template('policy.html')
+
+app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
