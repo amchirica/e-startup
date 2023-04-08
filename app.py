35f8396ff4dcf5db6ee792a81a5a2cd0fb69f34b
DariusMan22 <dariusmanolescu@yahoo.ro>
2023-04-08 21:15:08 +0300

    Main

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import os
-from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
@@ -41,16 +40,6 @@ def policy():
 app.route('/contact.html')
 def contact():
     return render_template('contact.html')
-
-app.route("/register")
-def register():
-    form = RegistrationForm()
-    return render_template('register.html', title = 'Register', form = form)
-
-app.route("/login")
-def register():
-    form = LoginForm()
-    return render_template('login.html', title = 'Login', form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
