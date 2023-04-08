13ed87179097fb906661c052bef3fb42715caa8b
DariusMan22 <dariusmanolescu@yahoo.ro>
2023-04-08 22:20:15 +0300

    E ok

--- app.py
+++ app.py
@@ -13,33 +13,30 @@ img = os.path.join('static', 'img')
 def homeindex():
     return render_template('index.html')
 
-@app.route('/blogslist.html')
-def blogslist():
-    return render_template('blogslist.html')
-
 @app.route('/blog.html')
-def blogspot():
+def blog():
     return render_template('blog.html')
 
 @app.route('/about.html')
 def about():
     return render_template('about.html')
 
-app.route('/login.html')
-def login():
-    return render_template('login.html')
 
-app.route('/project.html')
+@app.route('/project.html')
 def project():
     return render_template('project.html')
 
-app.route('/policy.html')
+@app.route('/policy.html')
 def policy():
     return render_template('policy.html')
 
-app.route('/contact.html')
+@app.route('/contact.html')
 def contact():
     return render_template('contact.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
